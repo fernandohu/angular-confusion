9260232dbce72c65e78c5535ee727f6b4562c314
Uchiyama <fernando.uchiyama@userede.com.br>
2018-04-27 16:39:17 -0300

    Header and footer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,6 +7,8 @@ import { FlexLayoutModule} from '@angular/flex-layout';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -14,7 +16,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent,
         MenuComponent,
-        DishdetailComponent
+        DishdetailComponent,
+        HeaderComponent,
+        FooterComponent
       ],
       imports: [
         BrowserModule,
